921f6a7610fef5b1cbd36d5c9a458a0111020f8d
mitchmorrison2 <mitchm@smu.edu>
2020-02-01 19:28:08 -0600

    size GUI changes

--- venv/GUI.py
+++ venv/GUI.py
@@ -2,19 +2,24 @@ import PySimpleGUI as sg
 
 sg.theme('DarkAmber')	# Add a touch of color
 # All the stuff inside your window.
-layout = [  [sg.Text('Brightness Modifier'), sg.Button('On'), sg.Button('Off')],
-            [sg.Text('Time interval between breaks: '), sg.InputText(), sg.Button('Ok')],
-            [sg.Button('Close')]]
+
+
+layout = [  [sg.Text('Brightness Modifier', size=(30,1)), sg.Button('On'), sg.Button('Off')],
+            [sg.Text('Time interval between breaks (min): ', size=(30,1)), sg.InputText('20', size=(10,15))],
+            [sg.Text('Break period (sec): ', size=(30,1)), sg.InputText('20', size=(10,15))],
+            [sg.Button('Ok'), sg.Button('Close')]]
 
 # Create the Window
-window = sg.Window('Protect Your Eyes!', layout)
+
+window = sg.Window('Protect Your Eyes!', layout, default_element_size = (40, 1))
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
     if event in (None, 'Close'):	# if user closes window or clicks cancel
         break
-    timeBtwnDim = values[0]
-    print(values[0])
+    break_time = values[0]
+    dim_screen_time = values[1]
+    print(values[0], values[1])
 #    print('You entered ', values[0])
 
 window.close()
