9fb23297ea9ca0514f343798ac6bb039d9c01ec7
mitchmorrison2 <mitchm@smu.edu>
2020-02-01 19:52:21 -0600

    button adds

--- venv/GUI.py
+++ venv/GUI.py
@@ -1,13 +1,13 @@
 import PySimpleGUI as sg
 
-sg.theme('DarkAmber')	# Add a touch of color
+sg.theme('Purple')	# Add a touch of color
 # All the stuff inside your window.
 
 
-layout = [  [sg.Text('Brightness Modifier', size=(30,1)), sg.Button('On'), sg.Button('Off')],
-            [sg.Text('Time interval between breaks (min): ', size=(30,1)), sg.InputText('20', size=(10,15))],
-            [sg.Text('Break period (sec): ', size=(30,1)), sg.InputText('20', size=(10,15))],
-            [sg.Button('Ok'), sg.Button('Close')]]
+layout = [  [sg.Text('Brightness Modifier', size=(30,1), font='Default 18'), sg.Button('On', font='Default 18'), sg.Button('Off', font='Default 18')],
+            [sg.Text('Time interval between breaks (min): ',font='Default 18', size=(30,1)), sg.InputText('20', font='Default 18',size=(10,15))],
+            [sg.Text('Break period (sec): ', font='Default 18', size=(30,1)), sg.InputText('20',font='Default 18', size=(10,15))],
+            [sg.Button('Close', font='Default 18')]]
 
 # Create the Window
 
@@ -17,9 +17,14 @@ while True:
     event, values = window.read()
     if event in (None, 'Close'):	# if user closes window or clicks cancel
         break
+    if event in (None, 'On'):
+        window['On'].update(disabled=True)
+        window['Off'].update(disabled=False)
+    if event in (None, 'Off'):
+        window['On'].update(disabled=False)
+        window['Off'].update(disabled=True)
     break_time = values[0]
     dim_screen_time = values[1]
     print(values[0], values[1])
-#    print('You entered ', values[0])
 
 window.close()
